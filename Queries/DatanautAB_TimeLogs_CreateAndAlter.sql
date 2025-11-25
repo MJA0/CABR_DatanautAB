7f3a8c1c78d3ff319edee0ff922489b7df516837
MJA0 <adchariya.c@gmail.com>
2025-11-25 15:17:26 +0100

    Altered TimeLogs with constraint for FKActivity

--- Queries/DatanautAB_TimeLogs_CreateAndAlter.sql
+++ Queries/DatanautAB_TimeLogs_CreateAndAlter.sql
@@ -3,11 +3,11 @@ CREATE TABLE TimeLogs(
 	LogDate DATETIME not null,
 	TimeSpent TIME(0),
 	FKProjectID INT,
-	FKActivity INT not null FOREIGN KEY(FKActivity) REFERENCES Activity(ActivityID),
+	FKActivity INT not null CONSTRAINT Fk_Timelogs_Acitvity FOREIGN KEY(FKActivity) REFERENCES Activity(ActivityID),
 	FKTeamMemberID INT
 );
 
--- TODO: Add these after Project and TeamMember is in the database
+----TODO: Add these after Project and TeamMember is in the database
 --ALTER TABLE TimeLogs
 --ADD CONSTRAINT Fk_TimeLogs_Project
 --FOREIGN KEY(FKProjectID) REFERENCES Project(ProjectID);
@@ -15,3 +15,8 @@ CREATE TABLE TimeLogs(
 --ALTER TABLE TimeLogs
 --ADD CONSTRAINT Fk_TimeLogs_TeamMember
 --FOREIGN KEY(FKTeamMemberID) REFERENCES TeamMember(TeamMemberID);
+
+----If no constraint is on FKActivity run this
+--ALTER TABLE TimeLogs
+--ADD CONSTRAINT Fk_Timelogs_Acitvity
+--FOREIGN KEY(FKActivity) REFERENCES Activity(ActivityID);
