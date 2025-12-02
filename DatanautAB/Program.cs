7aad949a11490201933639c8606c885bbcd3cf65
niklas eriksson <niklaseriksson32@hotmail.com>
2025-12-02 14:52:54 +0100

    add folders

---
+++ DatanautAB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DatanautAB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wdalösndmpnwa!");
+        }
+    }
+}
