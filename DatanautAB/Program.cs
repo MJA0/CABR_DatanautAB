9bb793bb2a292b097f6018f132e7b866ef808bde
MJA0 <adchariya.c@gmail.com>
2025-12-16 22:43:06 +0100

    Removed dublicates

--- DatanautAB/Program.cs
+++ DatanautAB/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DatanautAB.Data;
+using DatanautAB.Repositories;
 using DatanautAB.UI.MainMenu;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -7,12 +8,6 @@ using System.Reflection;
 using System.Text.RegularExpressions;
 using DatanautAB.DataSeed;
 using DatanautAB.Models;
-using DatanautAB.UI.MainMenu;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
-using System.Text.RegularExpressions;
 
 namespace DatanautAB
 {
@@ -38,23 +33,6 @@ namespace DatanautAB
 
             services.AddDbContext<DatanautContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("EducationProgramDB")));
-
-            var provider = services.BuildServiceProvider();
-
-            // Resolve DbContext
-            using var db = provider.GetRequiredService<DatanautContext>();
-
-            MainMenuUI.Show(db);
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(appRoot)  // AppContext.BaseDirectory points to the runtime folder
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-
-            // Setup DI container
-            var services = new ServiceCollection();
-
-            services.AddDbContext<DatanautContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("DatanautDB")));
 
             var provider = services.BuildServiceProvider();
 
